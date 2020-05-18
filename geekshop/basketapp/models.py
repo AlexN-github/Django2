0a9ba6ac39a56cacf01a1b77467ff649a9dc093d
Alex <n_alex2001@mail.ru>
2020-05-18 14:52:30 +0300

    Выполнено

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from django.conf import settings
 from mainapp.models import Product
 
+class BasketQuerySet(models.QuerySet):
+
+   def delete(self, *args, **kwargs):
+       for object in self:
+           object.product.quantity += object.quantity
+           object.product.save()
+       super(BasketQuerySet, self).delete(*args, **kwargs)
+
 class Basket(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
-    
+    objects = BasketQuerySet.as_manager()
     
     def _get_product_cost(self):
         "return cost of all products this type"
@@ -47,3 +55,18 @@ class Basket(models.Model):
         basket_items = cls.get_items(user)
         basket_items_dic = {}
         [basket_items_dic.update(({item.product: item.quantity}) for item in basket_items)]
+
+    def delete(self):
+        self.product.quantity += self.quantity
+        self.product.save()
+        super(self.__class__, self).delete()
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            print(self.__class__)
+            self.product.quantity -= self.quantity - \
+                                    self.__class__.get_item(self.pk).quantity
+        else:
+            self.product.quantity -= self.quantity
+        self.product.save()
+        super(self.__class__, self).save(*args, **kwargs)
